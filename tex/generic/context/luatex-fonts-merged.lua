85d005e999920a82d104e89012385adb864d7a10
Hans Hagen <pragma@wxs.nl>
2010-12-02 08:51:00 +0100

    beta 2010.12.02 08:51

--- tex/generic/context/luatex-fonts-merged.lua
+++ tex/generic/context/luatex-fonts-merged.lua
@@ -1,6 +1,6 @@
 -- merged file : luatex-fonts-merged.lua
 -- parent file : luatex-fonts.lua
--- merge date  : 12/02/10 00:11:28
+-- merge date  : 12/02/10 08:51:01
 
 do -- begin closure to overcome local limits and interference
 
