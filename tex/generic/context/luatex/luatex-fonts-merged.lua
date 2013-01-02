250316e74df7a2fe9544318ed39fd2f6621e8dbb
Marius <mariausol@gmail.com>
2013-01-02 19:40:16 +0200

    beta 2013.01.02 18:19

--- tex/generic/context/luatex/luatex-fonts-merged.lua
+++ tex/generic/context/luatex/luatex-fonts-merged.lua
@@ -1,6 +1,6 @@
 -- merged file : luatex-fonts-merged.lua
 -- parent file : luatex-fonts.lua
--- merge date  : 01/02/13 17:15:54
+-- merge date  : 01/02/13 18:19:13
 
 do -- begin closure to overcome local limits and interference
 
