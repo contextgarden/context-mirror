abc4c5b8c54f5768e2c494e7925dddb1d99850be
Hans Hagen <pragma@wxs.nl>
2014-01-06 18:10:00 +0100

    beta 2014.01.06 18:10

--- tex/context/base/font-ott.lua
+++ tex/context/base/font-ott.lua
@@ -42,6 +42,7 @@ local scripts = allocate {
     ['cprt'] = 'cypriot syllabary',
     ['cyrl'] = 'cyrillic',
     ['deva'] = 'devanagari',
+    ['dev2'] = 'devanagari variant 2',
     ['dsrt'] = 'deseret',
     ['ethi'] = 'ethiopic',
     ['geor'] = 'georgian',
@@ -67,6 +68,7 @@ local scripts = allocate {
     ['linb'] = 'linear b',
     ['math'] = 'mathematical alphanumeric symbols',
     ['mlym'] = 'malayalam',
+    ['mlm2'] = 'malayalam variant 2',
     ['mong'] = 'mongolian',
     ['musc'] = 'musical symbols',
     ['mymr'] = 'myanmar',
@@ -631,6 +633,7 @@ local features = allocate {
     ['js..'] = 'justification ..',
 
     ["dv.."] = "devanagari ..",
+    ["ml.."] = "malayalam ..",
 }
 
 local baselines = allocate {
