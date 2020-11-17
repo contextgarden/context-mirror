018176e75a7b4f3aeeb053cc31928cbaff8442f4
Hans Hagen <pragma@wxs.nl>
2020-11-17 13:26:53 +0100

    2020-11-17 12:42:00

--- tex/generic/context/luatex/luatex-fonts-merged.lua
+++ tex/generic/context/luatex/luatex-fonts-merged.lua
@@ -1,6 +1,6 @@
 -- merged file : c:/data/develop/context/sources/luatex-fonts-merged.lua
 -- parent file : c:/data/develop/context/sources/luatex-fonts.lua
--- merge date  : 2020-11-16 19:37
+-- merge date  : 2020-11-17 12:39
 
 do -- begin closure to overcome local limits and interference
 
